7f24752103a78714869dac3e1c358abf53d5a74d
AnnaShell <anna.tityushina@yandex.ru>
2025-06-24 15:38:24 +0300

    content markup

--- src/components/CourseCard/CourseCard.jsx
+++ src/components/CourseCard/CourseCard.jsx
@@ -6,9 +6,11 @@ function CourseCard({ course }) {
     return (
         <div className="course-card">
             <div>
-                <Link to={`/course/${course.id}`}>
-                    <h3>{course.title}</h3>
-                    <h4>{course.description}</h4>
+                <Link to={`/course/${course.id}`} className="course-card-link">
+                    <button className="course-card-button">
+                        <h3>{course.title}</h3>
+                        <h4>{course.description}</h4>
+                    </button>
                 </Link>
             </div>
         </div>
