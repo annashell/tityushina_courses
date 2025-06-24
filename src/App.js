1791a88369510470969ee0c9085373ea330a9ddb
AnnaShell <anna.tityushina@yandex.ru>
2025-06-24 15:55:25 +0300

    lm 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import CoursePage from './pages/CoursePage/CoursePage';
 import ChapterPage from './pages/ChapterPage/ChapterPage';
@@ -6,22 +6,23 @@ import Header from './components/Header/Header';
 import './App.css';
 
 function App() {
-  return (
-      <Router>
-        <div className="app">
-          <Header />
-          <div className="main-content">
-            <div className="content-area">
-              <Routes>
-                <Route path="/course/:courseId/chapter/:chapterId" element={<ChapterPage />} />
-                <Route path="/course/:courseId" element={<CoursePage />} />
-                <Route path="/" element={<HomePage />} />
-              </Routes>
+    return (
+        <Router>
+            <div className="app">
+                <Header/>
+                <div className="main-content">
+                    <div className="content-area">
+                        <Routes>
+                            <Route path="/course/:courseId/chapter/:chapterId" element={<ChapterPage/>}/>
+                            <Route path="/course/:courseId" element={<CoursePage/>}/>
+                            <Route path="/tityushina_courses/" element={<HomePage/>}/>
+                            <Route path="/" element={<HomePage/>}/>
+                        </Routes>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </Router>
-  );
+        </Router>
+    );
 }
 
 export default App;
